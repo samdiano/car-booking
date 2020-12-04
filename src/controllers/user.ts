292c21532f646b6a9e872c637b20755f34ad8375
Samuel George <samuegeorge58@gmail.com>
2020-12-04 06:06:51 +0100

    add tests for login endpoint

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -8,17 +8,20 @@ interface user {
     email: string;
     id: Number;
 }
+
+const secret = process.env.JWT_SECRET || "";
+
 const signIn = (req: Request, res: Response) => {
     const emailValidate = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     if (!req.body.email) {
-        return res.status(400).send({
+        return res.status(400).json({
             success: 'false',
             message: 'Email is required',
         });
     }
     if (!(req.body.email.match(emailValidate))) {
-        return res.status(400).send({
+        return res.status(400).json({
             success: 'false',
             message: 'Please enter a valid email address',
         });
@@ -26,14 +29,14 @@ const signIn = (req: Request, res: Response) => {
 
     models.User.findOne({ where: { email: req.body.email } }).then((user: user) => {
         if (!user) {
-            return res.status(404).json({ message: 'Invalid Email'});
+            return res.status(400).json({ message: 'Invalid Email' });
         }
         const token = jwt.sign({
             id: user.id,
             firstName: user.firstName,
             lastName: user.lastName,
             email: user.email,
-        }, "secret", { expiresIn: '24h' });
+        }, secret, { expiresIn: '24h' });
         return res.status(200).send({
             success: 'true',
             message: 'User logged in successfully',
