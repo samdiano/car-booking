ba375d21ebb4c8f5a6fbdbbe5b830b304b4e2b8b
Samuel George <samuegeorge58@gmail.com>
2020-12-04 17:28:02 +0100

    add middleware to validate token

---
+++ src/middlewares/validateToken.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+export default (req, res, next) => {
+    const token = req.header('x-auth-token');
+    if (!token) {
+        return res.status(401).json({
+            message: 'Access denied, no token provided'
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || "");
+        req.user = decoded;
+        next();
+    } catch (exception) {
+        res.status(400).json({
+            message: 'Invalid token'
+        });
+    }
+};
