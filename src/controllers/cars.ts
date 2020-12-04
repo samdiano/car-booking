f32feb33a10fc97814ac41d2516f12118cd32c5e
Samuel George <samuegeorge58@gmail.com>
2020-12-04 18:12:03 +0100

    add cars and tokenm validation test

--- src/controllers/cars.ts
+++ src/controllers/cars.ts
@@ -10,20 +10,7 @@ interface Car {
 }
 
 const getCars = (req: Request, res: Response) => {
-    const emailValidate = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if (!req.body.email) {
-        return res.status(400).json({
-            success: 'false',
-            message: 'Email is required',
-        });
-    }
-    if (!(req.body.email.match(emailValidate))) {
-        return res.status(400).json({
-            success: 'false',
-            message: 'Please enter a valid email address',
-        });
-    }
 
     models.Car.findAll()
         .then(cars => {
---
+++ src/tests/cars.test.ts
@@ -0,0 +1,49 @@
+import chai, { expect } from 'chai';
+import jwt from 'jsonwebtoken';
+import chaiHttp from 'chai-http';
+import server from '../server';
+
+chai.use(chaiHttp);
+
+describe('Users', () => {
+    const token = jwt.sign({
+        "id": 1,
+        "firstName": "Kleon",
+        "lastName": "Sleigh",
+        "email": "ksleigh1@china.com.cn",
+    }, process.env.JWT_SECRET || "", { expiresIn: '24h' })
+
+    it('Should fetch all cars', (done) => {
+        chai.request(server)
+            .get('/api/v1/cars')
+            .set("x-auth-token", token)
+            .end((err, res) => {
+                expect(res.status).to.equal(200);
+                expect(res.body).to.have.property('message').equal('Cars retrieved successfully');
+                done();
+            });
+    });
+
+    it('Should validate token', (done) => {
+        chai.request(server)
+            .get('/api/v1/cars')
+            .set("x-auth-token", token+"oo")
+            .end((err, res) => {
+                expect(res.status).to.equal(400);
+                expect(res.body).to.have.property('message').equal('Invalid token');
+                done();
+            });
+    });
+
+    it('Should throw error for empty token', (done) => {
+        chai.request(server)
+            .get('/api/v1/cars')
+            .end((err, res) => {
+                expect(res.status).to.equal(401);
+                expect(res.body).to.have.property('message').equal('Access denied, no token provided');
+                done();
+            });
+    });
+
+}
+)
